18652657dfe18fb4ff6019652d2699be9b943836
DeliveryLeleka <deliveryleleka@gmail.com>
2026-09-16 14:18:14 +0300

    new favicon + title change

--- app/root.tsx
+++ app/root.tsx
@@ -29,7 +29,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Leleka</title>
+        <title>Поштовий Лелека</title>
         <Meta />
         <Links />
       </head>
